ae2462c67cbc3fa210f609693b2de932c6e49715
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-07 15:00:32 -0600

    add new blueprint for /services route and stub template html

--- lawn_care/views/service_views.py
+++ lawn_care/views/service_views.py
@@ -1,5 +1,7 @@
 import flask
 
+from lawn_care.services import services_service
+
 blueprint = flask.Blueprint('service', __name__, template_folder='templates')
 
 
@@ -7,3 +9,9 @@ blueprint = flask.Blueprint('service', __name__, template_folder='templates')
 def show_service(service_name: str):
     ret_dict = {'name': service_name}
     return flask.render_template('services/display.html', **ret_dict)
+
+@blueprint.route('/services')
+def all_services():
+    services = services_service.get_all_services()
+    ret_dict = {'services': services}
+    return flask.render_template('services/all.html', **ret_dict)
