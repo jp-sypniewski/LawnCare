2b42a913bc3803af01490826abe9b0c7bdac5083
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-07 16:19:57 -0600

    update readme, stub more routes in account_views

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## LawnCare
 
-Flask MySQL/SQLAlchemy data driven app for lawn services
+### A full stack web application with Flask
+
+
+#### Description
+
+LawnCare is a template full stack web application.
+
+The application is built to test skill in the Flask framework while using SQLAlchemy on a MySQL database.
+
+
+#### Technologies
+
+* Flask
+* SQLAlchemy
+* MySQL
--- lawn_care/views/account_views.py
+++ lawn_care/views/account_views.py
@@ -3,6 +3,23 @@ import flask
 blueprint = flask.Blueprint('account', __name__, template_folder='templates')
 
 
-@blueprint.route('/login')
-def show_login():
+@blueprint.route('/account')
+def account():
+    return flask.render_template('account/account.html')
+
+
+@blueprint.route('/account/login', methods=['GET'])
+def login_get():
     return flask.render_template('account/login.html')
+
+
+@blueprint.route('/account/login', methods=['POST'])
+def login_post():
+    return flask.render_template('account/login.html')
+
+
+@blueprint.route('/account/logout')
+def logout():
+    resp = flask.redirect('/')
+    # remove the user's cookie once it's made
+    return resp
