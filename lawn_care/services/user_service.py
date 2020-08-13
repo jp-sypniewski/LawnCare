9a13a75c5f4b205bd8440d0211aac172535e7209
jp-sypniewski <jpsypniewski@icloud.com>
2020-08-13 15:34:07 -0600

    add passlib to requirements, add functions for hashing password, update create_user to hash password

--- lawn_care/services/user_service.py
+++ lawn_care/services/user_service.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from passlib.handlers.sha2_crypt import sha512_crypt as crypto
+
 from lawn_care.data import db_session
 from lawn_care.data.users import User
 
@@ -17,7 +19,7 @@ def create_user(username: str,
                 ):
     new_user = User()
     new_user.username = username
-    new_user.hashed_password = password
+    new_user.hashed_password = hash_text(password)
     new_user.email = email
     new_user.first_name = first_name
     new_user.last_name = last_name
@@ -30,3 +32,26 @@ def create_user(username: str,
         session.close()
 
     return new_user
+
+
+def hash_text(text: str) -> str:
+    hashed_text = crypto.encrypt(text, rounds=152701)
+    return hashed_text
+
+
+def verify_hash(hashed_text: str, plain_text: str) -> bool:
+    return crypto.verify(plain_text, hashed_text)
+
+
+def login_user(email, password) -> Optional[User]:
+    session = db_session.create_session()
+
+    user = session.query(User).filter(User.email == email).first()
+
+    if not user:
+        return None
+
+    if not verify_hash(user.hashed_password, password):
+        return None
+
+    return user
